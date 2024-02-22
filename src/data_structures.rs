40efd2824670b1c43dfbb6d4baccc5da629599a2
basyniae_laptop <152874900+basyniae@users.noreply.github.com>
2024-02-22 21:28:22 +0100

    Added build sequence (some weird logic),

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -72,4 +72,68 @@ impl Blocks {
             origin: self.origin,
         }
     }
+
+    // From a shape (which is assumed to be non-pathological, TODO: make checks for this)
+    // find the sequence of side lengths.
+    // Goal: easy construction in minecraft (it's how i think of circles)
+    pub fn get_build_sequence(&self) -> Vec<usize> {
+        // We scan horizontal lines bottom to top (which is just repeated addition of the edge length)
+        // Then subtract the nr blocks in each successive layer and divide by two
+
+        let mut nr_blocks_per_layer = vec![];
+
+        // loop over rows
+        'row: for i in 0..self.edge_length {
+            let mut nr_blocks_in_ith_layer:usize = 0;
+            let mut prev_block_had_block_below = false; // detect if we're accross the symmetry axis
+            let mut prev_location_had_block = false; // detect if all blocks on a row have been scanned
+            let mut last_row = false; // detect if this is the last row we need to include in the build list
+
+            // loop over elements of rows
+            for j in 0..self.edge_length {
+
+                // If there is a block at the current position
+                if self.blocks[i*self.edge_length + j] {
+                    prev_location_had_block = true;
+                    // And there is no block directly below it (note that padding takes care of positivity)
+                    if !self.blocks[(i-1)*self.edge_length + j] {
+                        if prev_block_had_block_below {
+                            // If the previous block had a block below it and this one doesn't,
+                            //  we must be past the symmetry axis. So no contribution and stop
+                            //  scanning this line.
+                            nr_blocks_per_layer.push(nr_blocks_in_ith_layer);
+                            if last_row {
+                                break 'row
+                            }
+                            break
+                        }
+                        // Then this block contributes to the build sequence
+                        nr_blocks_in_ith_layer += 1;
+                    } else {
+                        prev_block_had_block_below = true;
+                    }
+                    
+                    // If there is no block left-diagonally above the currently block
+                    //  we must be past the 45° point. (this assumes "niceness" of the input)
+                    if !self.blocks[(i+1)*self.edge_length + j -1] {
+                        // so break the outer loop as soon as we've scanned the full row
+                        // (this to avoid error in case of the final in build sequence being >1 in length)
+                        last_row = true;
+                    }
+
+                } else {
+                    // Stop scanning the line at the end of the line
+                    if prev_location_had_block {
+                        nr_blocks_per_layer.push(nr_blocks_in_ith_layer);
+                        if last_row {
+                            break 'row
+                        }
+                        break
+                    }
+                }
+            }
+        }
+
+        nr_blocks_per_layer
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// (ripped straight from the example)
 
 mod app;
 pub mod data_structures;
@@ -7,7 +9,6 @@ use eframe::egui::ViewportBuilder;
 use eframe::{Theme, HardwareAcceleration};
 
 fn main() {
-    println!("Hello, world!");
     let options = eframe::NativeOptions {
             viewport: ViewportBuilder::default(),
             vsync: false,
@@ -17,7 +18,7 @@ fn main() {
             hardware_acceleration: HardwareAcceleration::Preferred,
             renderer: Default::default(),
             follow_system_theme: false,
-            default_theme: Theme::Dark,
+            default_theme: Theme::Dark, 
             run_and_return: false,
             event_loop_builder: None,
             shader_version: None,
@@ -28,7 +29,7 @@ fn main() {
 
     // Use `let _`  to ignore the result properly...
     let _ = eframe::run_native(
-        "Voxel circles", 
+        "Voxircles", 
         options, 
         Box::new(|cc|  Box::new(crate::app::App::new(cc)))
     );
