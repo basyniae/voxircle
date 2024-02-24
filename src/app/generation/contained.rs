958d50054be1644731a85607fcacd795edf181f4
basyniae_laptop <152874900+basyniae@users.noreply.github.com>
2024-02-24 13:49:21 +0100

    autoformatting + reenable build sequence

--- src/app/generation/contained.rs
+++ src/app/generation/contained.rs
@@ -1,30 +1,30 @@
 use crate::data_structures::Blocks;
 
 pub fn generate_alg_contained(radius: f64, center_offset_x: f64, center_offset_y: f64) -> Blocks {
-    let edge_length = ((2.0*radius).ceil() as usize) + 4; // the 4 is needed as a buffer.. 
+    let edge_length = ((2.0 * radius).ceil() as usize) + 4; // the 4 is needed as a buffer..
     // i think we're able to get away with less but it doesn't matter. Buffer is required to make the interior work as expected
-    let origin = [(edge_length / 2) as f64, (edge_length / 2) as f64]; 
+    let origin = [(edge_length / 2) as f64, (edge_length / 2) as f64];
     // in bitmatrix coordinates, where is the center of the grid?
     let mut output_vec = Vec::new();
 
     // The above part is the same for all algorithms (I think at this stage)
 
-
-    for i in 0..edge_length.pow(2) { // loop over all coords
+    for i in 0..edge_length.pow(2) {
+        // loop over all coords
         // Bottom right coordinate of the box in bitmatrix coordinates is [i % edge_length, i / edge_length]
         // We have that the box is contained in the disk <=> all corner of the box are in the disk
         // Want to get at the distance from the corners of a box to the origin + offset (we do this component-wise)
         let d_x_left = ((i % edge_length) as f64) - (origin[0] + center_offset_x);
-        let d_x_right = ((i % edge_length) as f64 + 1.0) - (origin[0] + center_offset_x);
+        let d_x_right = ((i % edge_length) as f64) + 1.0 - (origin[0] + center_offset_x);
         let d_y_bottom = ((i / edge_length) as f64) - (origin[1] + center_offset_y);
-        let d_y_top = ((i / edge_length) as f64 + 1.0) - (origin[1] + center_offset_y);
+        let d_y_top = ((i / edge_length) as f64) + 1.0 - (origin[1] + center_offset_y);
         // Easier to look at the squared distance
         output_vec.push(
-            (d_x_left.powi(2) + d_y_bottom.powi(2) <= radius.powi(2))
-            && (d_x_right.powi(2) + d_y_bottom.powi(2) <= radius.powi(2))
-            && (d_x_left.powi(2) + d_y_top.powi(2) <= radius.powi(2))
-            && (d_x_right.powi(2) + d_y_top.powi(2) <= radius.powi(2))
-        )
+            d_x_left.powi(2) + d_y_bottom.powi(2) <= radius.powi(2) &&
+                d_x_right.powi(2) + d_y_bottom.powi(2) <= radius.powi(2) &&
+                d_x_left.powi(2) + d_y_top.powi(2) <= radius.powi(2) &&
+                d_x_right.powi(2) + d_y_top.powi(2) <= radius.powi(2)
+        );
     }
 
     Blocks {
--- src/data_structures.rs
+++ src/data_structures.rs
@@ -4,7 +4,7 @@
 pub struct Blocks {
     pub blocks: Vec<bool>,
     pub edge_length: usize, // of type usize because we index with it a bunch of times
-    pub origin: [f64; 2] // At which bitmatrix coords is the center of the circle? 
+    pub origin: [f64; 2], // At which bitmatrix coords is the center of the circle?
     // For a block-diameter 4 circle it would be [2.0,2.0], for a block-diameter 5 circle it would be [2.5,2.5]
     // but how does this work paradigmatically
 }
@@ -18,13 +18,13 @@ impl Blocks {
         for b in &self.blocks {
             if *b {
                 output_vec.push([
-                    (i % self.edge_length) as f64 - self.origin[0], // Get integer x position (which is bot. left), then make origin center
-                    (i / self.edge_length) as f64 - self.origin[1]
-                    ])
+                    ((i % self.edge_length) as f64) - self.origin[0], // Get integer x position (which is bot. left), then make origin center
+                    ((i / self.edge_length) as f64) - self.origin[1],
+                ]);
             }
             i += 1;
         }
-        
+
         output_vec
     }
 
@@ -44,20 +44,24 @@ impl Blocks {
         let mut output_vec = Vec::new();
 
         for b in &self.blocks {
-            
             if *b == false {
                 // if not a point in the set of blocks, then certainly not a point in the interior
                 output_vec.push(false);
-            } else if (i % self.edge_length == 0) 
-            || (i % self.edge_length == self.edge_length-1
-            || (i / self.edge_length == 0)
-            || (i / self.edge_length == self.edge_length-1)) {
+            } else if
+                i % self.edge_length == 0 ||
+                i % self.edge_length == self.edge_length - 1 ||
+                i / self.edge_length == 0 ||
+                i / self.edge_length == self.edge_length - 1
+            {
                 // If on the boundary, output not-interior (valid via judicious padding of the interesting structure)
                 output_vec.push(false);
-            } else if (self.blocks[i+1] == false)
-            || (self.blocks[i-1] == false)
-            || (self.blocks[i+self.edge_length] == false)
-            || (self.blocks[i-self.edge_length] == false) { // actually check the neighbors
+            } else if
+                self.blocks[i + 1] == false ||
+                self.blocks[i - 1] == false ||
+                self.blocks[i + self.edge_length] == false ||
+                self.blocks[i - self.edge_length] == false
+            {
+                // actually check the neighbors
                 output_vec.push(false);
             } else {
                 output_vec.push(true);
@@ -84,51 +88,49 @@ impl Blocks {
 
         // loop over rows
         'row: for i in 0..self.edge_length {
-            let mut nr_blocks_in_ith_layer:usize = 0;
+            let mut nr_blocks_in_ith_layer: usize = 0;
             let mut prev_block_had_block_below = false; // detect if we're accross the symmetry axis
             let mut prev_location_had_block = false; // detect if all blocks on a row have been scanned
             let mut last_row = false; // detect if this is the last row we need to include in the build list
 
             // loop over elements of rows
             for j in 0..self.edge_length {
-
                 // If there is a block at the current position
-                if self.blocks[i*self.edge_length + j] {
+                if self.blocks[i * self.edge_length + j] {
                     prev_location_had_block = true;
                     // And there is no block directly below it (note that padding takes care of positivity)
-                    if !self.blocks[(i-1)*self.edge_length + j] {
+                    if !self.blocks[(i - 1) * self.edge_length + j] {
                         if prev_block_had_block_below {
                             // If the previous block had a block below it and this one doesn't,
                             //  we must be past the symmetry axis. So no contribution and stop
                             //  scanning this line.
                             nr_blocks_per_layer.push(nr_blocks_in_ith_layer);
                             if last_row {
-                                break 'row
+                                break 'row;
                             }
-                            break
+                            break;
                         }
                         // Then this block contributes to the build sequence
                         nr_blocks_in_ith_layer += 1;
                     } else {
                         prev_block_had_block_below = true;
                     }
-                    
+
                     // If there is no block left-diagonally above the currently block
                     //  we must be past the 45° point. (this assumes "niceness" of the input)
-                    if !self.blocks[(i+1)*self.edge_length + j -1] {
+                    if !self.blocks[(i + 1) * self.edge_length + j - 1] {
                         // so break the outer loop as soon as we've scanned the full row
                         // (this to avoid error in case of the final in build sequence being >1 in length)
                         last_row = true;
                     }
-
                 } else {
                     // Stop scanning the line at the end of the line
                     if prev_location_had_block {
                         nr_blocks_per_layer.push(nr_blocks_in_ith_layer);
                         if last_row {
-                            break 'row
+                            break 'row;
                         }
-                        break
+                        break;
                     }
                 }
             }
