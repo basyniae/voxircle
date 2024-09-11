01050970a503adf2cbb589bc26bd668be34a54d8
Iris <152874900+basyniae@users.noreply.github.com>
2024-09-11 23:03:05 +0200

    Factor the actual process of generation out of the ui component

--- src/app/ui_generation.rs
+++ src/app/ui_generation.rs
@@ -1,21 +1,18 @@
 use eframe::egui::Ui;
 use mlua::Lua;
 
-use crate::app::data_structures::blocks::Blocks;
 use crate::app::data_structures::gen_config::GenConfig;
 use crate::app::data_structures::zvec::ZVec;
 use crate::app::lua_field::LuaField;
 
-// TODO: Only auto generate if the values have changed
 pub fn ui_generation(
     ui: &mut Ui,
-    auto_generate_current_layer: &mut bool,
-    auto_generate_all_layers: &mut bool,
+    generate_current_layer: &mut bool,
+    generate_all_layers: &mut bool,
     circle_mode: bool,
     layer_mode: bool,
     lua_mode: bool,
     stack_gen_config: &mut ZVec<GenConfig>,
-    stack_blocks: &mut ZVec<Blocks>,
     lua: &mut Lua,
     lua_field_radius_a: &mut LuaField,
     lua_field_radius_b: &mut LuaField,
@@ -26,15 +23,7 @@ pub fn ui_generation(
     layer_lowest: isize,
     layer_highest: isize,
     current_layer: isize,
-    recompute_metrics: &mut bool,
 ) {
-    if layer_mode {
-        ui.checkbox(auto_generate_current_layer, "Auto-generate current layer");
-        ui.checkbox(auto_generate_all_layers, "Auto-generate all layers");
-    } else {
-        ui.checkbox(auto_generate_current_layer, "Auto-generate");
-    }
-
     if lua_mode {
         ui.columns(2, |columns| {
             if columns[0]
@@ -42,7 +31,7 @@ pub fn ui_generation(
                 .clicked()
             {
                 set_parameters_for_layer(
-                    stack_gen_config,
+                    &mut stack_gen_config.get_mut(current_layer).unwrap(),
                     lua,
                     lua_field_radius_a,
                     lua_field_radius_b,
@@ -53,23 +42,9 @@ pub fn ui_generation(
                     circle_mode,
                     current_layer,
                 );
-
-                lua_field_radius_a.register_success();
-                lua_field_radius_b.register_success();
-                lua_field_tilt.register_success();
-                lua_field_squircle_parameter.register_success();
-                lua_field_center_offset_x.register_success();
-                lua_field_center_offset_y.register_success();
             }
             columns[0].centered_and_justified(|ui| {
-                if ui.button("Generate current layer").clicked() || *auto_generate_current_layer {
-                    stack_blocks.set(
-                        current_layer,
-                        stack_gen_config.get_mut(current_layer).unwrap().generate(),
-                    );
-
-                    *recompute_metrics = true;
-                }
+                *generate_current_layer = ui.button("Generate current layer").clicked();
             });
 
             if columns[1]
@@ -78,7 +53,7 @@ pub fn ui_generation(
             {
                 for layer in layer_lowest..=layer_highest {
                     set_parameters_for_layer(
-                        stack_gen_config,
+                        &mut stack_gen_config.get_mut(layer).unwrap(),
                         lua,
                         lua_field_radius_a,
                         lua_field_radius_b,
@@ -90,87 +65,32 @@ pub fn ui_generation(
                         layer,
                     )
                 }
-
-                lua_field_radius_a.register_success();
-                lua_field_radius_b.register_success();
-                lua_field_tilt.register_success();
-                lua_field_squircle_parameter.register_success();
-                lua_field_center_offset_x.register_success();
-                lua_field_center_offset_y.register_success();
             }
             columns[1].centered_and_justified(|ui| {
-                if ui.button("Generate all layers").clicked() || *auto_generate_all_layers {
-                    generate_all_layers(
-                        stack_gen_config,
-                        stack_blocks,
-                        layer_lowest,
-                        recompute_metrics,
-                    )
-                }
+                *generate_all_layers = ui.button("Generate all layers").clicked();
             });
         });
     } else {
         if layer_mode {
             ui.columns(2, |columns| {
                 columns[0].centered_and_justified(|ui| {
-                    if ui.button("Generate current layer").clicked() || *auto_generate_current_layer
-                    {
-                        stack_blocks.set(
-                            current_layer,
-                            stack_gen_config.get_mut(current_layer).unwrap().generate(),
-                        );
-
-                        *recompute_metrics = true;
-                    }
+                    *generate_current_layer = ui.button("Generate current layer").clicked();
                 });
 
                 columns[1].centered_and_justified(|ui| {
-                    if ui.button("Generate all layers").clicked() || *auto_generate_all_layers {
-                        generate_all_layers(
-                            stack_gen_config,
-                            stack_blocks,
-                            layer_lowest,
-                            recompute_metrics,
-                        )
-                    }
+                    *generate_all_layers = ui.button("Generate all layers").clicked();
                 });
             })
         } else {
             ui.centered_and_justified(|ui| {
-                if ui.button("Generate").clicked() || *auto_generate_current_layer {
-                    stack_blocks.set(
-                        current_layer,
-                        stack_gen_config.get_mut(current_layer).unwrap().generate(),
-                    );
-
-                    *recompute_metrics = true;
-                }
+                *generate_current_layer = ui.button("Generate").clicked();
             });
         }
     }
 }
 
-fn generate_all_layers(
-    stack_gen_config: &mut ZVec<GenConfig>,
-    stack_blocks: &mut ZVec<Blocks>,
-    layer_lowest: isize,
-    recompute_metrics: &mut bool,
-) {
-    // Generate all layers
-    *stack_blocks = ZVec::new(
-        stack_gen_config
-            .data
-            .iter()
-            .map(|config| config.generate())
-            .collect(),
-        layer_lowest,
-    );
-
-    *recompute_metrics = true;
-}
-
 fn set_parameters_for_layer(
-    stack_gen_config: &mut ZVec<GenConfig>,
+    gen_config: &mut GenConfig,
     lua: &mut Lua,
     lua_field_radius_a: &mut LuaField,
     lua_field_radius_b: &mut LuaField,
@@ -184,28 +104,19 @@ fn set_parameters_for_layer(
     lua.globals().set("layer", layer).unwrap();
     lua.globals().set("l", layer).unwrap();
 
-    lua_field_radius_a.eval(lua, &mut stack_gen_config.get_mut(layer).unwrap().radius_a);
+    lua_field_radius_a.eval(lua, &mut gen_config.radius_a);
 
     if circle_mode {
-        lua_field_radius_a.eval(lua, &mut stack_gen_config.get_mut(layer).unwrap().radius_b);
+        lua_field_radius_a.eval(lua, &mut gen_config.radius_b);
     } else {
-        lua_field_radius_b.eval(lua, &mut stack_gen_config.get_mut(layer).unwrap().radius_b);
+        lua_field_radius_b.eval(lua, &mut gen_config.radius_b);
     }
 
-    lua_field_tilt.eval(lua, &mut stack_gen_config.get_mut(layer).unwrap().tilt);
+    lua_field_tilt.eval(lua, &mut gen_config.tilt);
 
-    lua_field_squircle_parameter.eval(
-        lua,
-        &mut stack_gen_config.get_mut(layer).unwrap().squircle_parameter,
-    );
+    lua_field_squircle_parameter.eval(lua, &mut gen_config.squircle_parameter);
 
-    lua_field_center_offset_x.eval(
-        lua,
-        &mut stack_gen_config.get_mut(layer).unwrap().center_offset_x,
-    );
+    lua_field_center_offset_x.eval(lua, &mut gen_config.center_offset_x);
 
-    lua_field_center_offset_y.eval(
-        lua,
-        &mut stack_gen_config.get_mut(layer).unwrap().center_offset_y,
-    );
+    lua_field_center_offset_y.eval(lua, &mut gen_config.center_offset_y);
 }
