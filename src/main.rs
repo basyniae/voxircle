02cf27c79c6f0af56f8212ddeb90febb868509c1
basyniae <iris.van.ram@gmail.com>
2024-12-23 11:05:56 +0100

    Working Trunk (webassembly) + Native

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,80 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-                                                                   // (ripped straight from the example)
+#![cfg_attr(
+    not(debug_assertions),
+    windows_subsystem = "windows"
+)] // hide console window on Windows in release
+// (ripped straight from the example)
 
 use std::default::Default;
-
-use eframe::egui::ViewportBuilder;
-use eframe::{HardwareAcceleration, Theme};
-
 mod app;
 
-fn main() {
-    let options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default(),
-        vsync: false,
-        multisampling: 0,
-        depth_buffer: 0,
-        stencil_buffer: 0,
-        hardware_acceleration: HardwareAcceleration::Preferred,
-        renderer: Default::default(),
-        follow_system_theme: false,
-        default_theme: Theme::Dark,
-        run_and_return: false,
-        event_loop_builder: None,
-        shader_version: None,
-        centered: false,
-        window_builder: Default::default(),
-        persist_window: false,
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([800.0, 600.0]) // window size when starting
+            .with_min_inner_size([300.0, 220.0]) // smallest window size possible with resizing
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
     };
 
-    // Use `let _`  to ignore the result properly...
-    let _ = eframe::run_native(
+    eframe::run_native(
         "Voxircle",
-        options,
-        Box::new(|cc| Box::new(app::App::new(cc))),
-    );
+        native_options,
+        Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+    )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    // panic!("running main() for wasm32 architecture");
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+            )
+            .await;
+
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
