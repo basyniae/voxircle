169d80b847572997a791a4399dd661ffd8890e97
Iris <152874900+basyniae@users.noreply.github.com>
2025-02-20 14:42:00 +0100

    Bug squash for not capturing the shape in the grid for large offsets

--- src/app/sampling/sampled_parameters.rs
+++ src/app/sampling/sampled_parameters.rs
@@ -36,8 +36,6 @@ impl Default for LayerParameters {
 }
 
 impl LayerParameters {
-    // fixme: make grid size determination better: needs to also take care of the offset (if
-    //  the input is very offset this method fails)
     /// Run the generation algorithm for the configuration `self`, the output is a `Blocks` object. document.
     pub fn generate(&self, sample_combine_method: &SampleCombineMethod) -> Blocks {
         // Determine grid size
@@ -45,6 +43,8 @@ impl LayerParameters {
         //  around the generated figure. Assuming a square figure (squircle parameter infinity), we
         //  need an x side length of 2.0 * sqrt(2) * radius_major. Add 4 for a padding of at least 2
         //  on each side.
+
+        // Compute the largest radii for all shapes on this layer
         let largest_radius_a = self
             .parameters
             .iter()
@@ -54,8 +54,22 @@ impl LayerParameters {
             .iter()
             .fold(f64::NEG_INFINITY, |a, b| a.max(b.radius_b));
 
-        let grid_size =
-            (2.0 * 1.42 * f64::max(largest_radius_a, largest_radius_b)).ceil() as usize + 4;
+        // Compute the largest offset for all shapes on this layer
+        let largest_offset_x = self
+            .parameters
+            .iter()
+            .fold(f64::NEG_INFINITY, |a, b| a.max(b.center_offset_x));
+        let largest_offset_y = self
+            .parameters
+            .iter()
+            .fold(f64::NEG_INFINITY, |a, b| a.max(b.center_offset_y));
+
+        // Note that this method works but is kind of stupid. Ideally we'd want to have a grid that's
+        //  a lot smaller but still contains all the shapes (it can't be centered at the origin then anymore)
+        let grid_size = ((2.0 * 1.42 * f64::max(largest_radius_a, largest_radius_b)).ceil()
+            + 2.0 * largest_offset_x.abs().max(largest_offset_y.abs()).ceil())
+            as usize
+            + 4;
 
         // Generate from circle with selected algorithm
         Blocks::combine(
